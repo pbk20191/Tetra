d9b34b311584d604a55ce7ff41e596cada6940da
pbk <pbk@iquest.kr>
2022-09-17 21:53:19 +0900

    컴바인에 Concurrency 작업을 주입하는 operator 추가

--- Sources/AsyncSugar/Combine+Concurrency.swift
+++ Sources/AsyncSugar/Combine+Concurrency.swift
@@ -10,6 +10,47 @@ import Combine
 
 public extension Publisher {
     
+    func mapTask<T:Sendable>(maxTaskCount:Subscribers.Demand = .max(1), transform: @escaping @Sendable (Output) async -> T) -> AnyPublisher<T,Never> where Failure == Never, Output:Sendable {
+        map{ output in
+            Task {
+                await withTaskCancellationHandler {
+                    Swift.print("\(#function) taskCancel")
+                } operation: {
+                    await transform(output)
+                }
+            }
+            
+        }
+        .flatMap(maxPublishers: maxTaskCount){ task in
+            Future<T,Never> { promise in
+                Task { await promise(task.result) }
+            }
+            .handleEvents(receiveCancel: task.cancel)
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func tryMapTask<T:Sendable>(maxTaskCount:Subscribers.Demand = .max(1), transform: @escaping @Sendable (Output) async throws -> T) -> AnyPublisher<T,Error> where Output:Sendable {
+        mapError{ $0 }
+            .map{ output in
+                Task {
+                    try await withTaskCancellationHandler {
+                        Swift.print("\(#function) taskCancel")
+                    } operation: {
+                        try await transform(output)
+                    }
+                }
+            }
+            .flatMap(maxPublishers: maxTaskCount){ task in
+                Future<T,Error> { promise in
+                    Task { await promise(task.result) }
+                }
+                .handleEvents(receiveCancel: task.cancel)
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    
     @available(iOS, deprecated: 15.0, renamed: "values")
     @available(macCatalyst, deprecated: 15.0, renamed: "values")
     @available(tvOS, deprecated: 15.0, renamed: "values")
@@ -17,10 +58,18 @@ public extension Publisher {
     @available(watchOS, deprecated: 8.0, renamed: "values")
     var asyncStream: AsyncThrowingStream<Output,Error> {
         if #available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 15.0, *) {
-            var iterator = values.makeAsyncIterator()
-            return AsyncThrowingStream<Output,Error> {
-                try Task.checkCancellation()
-                return try await iterator.next()
+            return AsyncThrowingStream<Output,Error>{[source = values] continuation in
+                let task = Task {
+                    do {
+                        for try await i in source {
+                            continuation.yield(i)
+                        }
+                        continuation.finish()
+                    } catch {
+                        continuation.finish(throwing: error)
+                    }
+                }
+                continuation.onTermination = { @Sendable _ in task.cancel() }
             }
         } else {
             return AsyncThrowingStream<Output, Error> { continuation in
@@ -59,10 +108,14 @@ public extension Publisher where Failure == Never {
     @available(watchOS, deprecated: 8.0, renamed: "values")
     var asyncStream:AsyncStream<Output> {
         if #available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, *) {
-            var iterator = values.makeAsyncIterator()
-            return AsyncStream<Output> {
-                guard !Task.isCancelled else { return nil }
-                return await iterator.next()
+           return AsyncStream(Output.self) { [source = values] continutation in
+                let task = Task {
+                    for await i in source {
+                        continutation.yield(i)
+                    }
+                    continutation.finish()
+                }
+                continutation.onTermination = { @Sendable _ in task.cancel() }
             }
         } else {
             return AsyncStream<Output> { continuation in
@@ -71,7 +124,6 @@ public extension Publisher where Failure == Never {
                 continuation.onTermination = { @Sendable _ in
                     onTermination()
                 }
-
                 cancellable = sink(
                     receiveCompletion: { _ in
                         continuation.finish()
