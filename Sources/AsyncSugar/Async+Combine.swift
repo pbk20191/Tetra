7c6c98bf82b3c2bb2e12ad4195c1d5867813d18a
pbk <pbk@iquest.kr>
2022-09-21 21:18:59 +0900

    CompatAsyncPublisher 구현체 수정( 내부 asyncStream에 의한 fatal error 케이스 대응)

--- Sources/AsyncSugar/Async+Combine.swift
+++ Sources/AsyncSugar/Async+Combine.swift
@@ -8,15 +8,17 @@
 import Foundation
 import Combine
 
-public extension AsyncStream {
-    var publisher:AnyPublisher<Element,Never> {
+public extension AsyncSequence {
+    
+    var omittedPublisher:AnyPublisher<Element,Never> {
         Just(self).flatMap(maxPublishers: .max(1)) { source in
             let subject = PassthroughSubject<Element,Never>()
             let task = Task {
                 await withTaskCancellationHandler {
-                    print("asyncStream cancelled")
+                    print("asyncSequence cancelled")
                 } operation: {
-                    for await i in source {
+                    var iterator = source.makeAsyncIterator()
+                    while let i = try? await iterator.next() {
                         subject.send(i)
                     }
                     subject.send(completion: .finished)
@@ -26,9 +28,7 @@ public extension AsyncStream {
             return subject.handleEvents(receiveCancel: task.cancel)
         }.eraseToAnyPublisher()
     }
-}
-
-public extension AsyncSequence {
+    
     var publisher:AnyPublisher<Element,Error> {
         if #available(iOS 14.0, tvOS 14.0, macCatalyst 14.0, watchOS 7.0, macOS 11.0, *) {
             return Just(self).flatMap(maxPublishers: .max(1)) { source in
@@ -76,87 +76,6 @@ public extension AsyncSequence {
 }
 
 
-fileprivate final class RefBox<T> {
-    var value:T?
-
-}
-
-
-public struct CompatAsyncThrowingPublisher<P:Publisher>: AsyncSequence where P.Failure:Error {
-
-    public typealias AsyncIterator = Iterator
-    public typealias Element = P.Output
-    
-    public var publisher:P
-    
-    public func makeAsyncIterator() -> AsyncIterator {
-        Iterator(source: publisher)
-    }
-    
-    public struct Iterator: AsyncIteratorProtocol {
-        
-        public typealias Element = P.Output
-        let source:P
-        private var base:AsyncThrowingStream<Element,Error>.Iterator? = nil
-        private let box = RefBox<Subscription>()
-        
-        mutating public func next() async throws -> P.Output? {
-            if base == nil {
-                base = setup()
-            }
-            if Task.isCancelled {
-                box.value?.cancel()
-            } else {
-                box.value?.request(.max(1))
-            }
-            return try await base?.next()
-        }
-        
-        fileprivate init(source: P) {
-            self.source = source
-
-        }
-        
-        private func setup() -> AsyncThrowingStream<Element,Error>.Iterator {
-            let sema = DispatchSemaphore(value: 0)
-            
-            let iterator = AsyncThrowingStream<Element,Error>{ continuation in
-                let subscriber = AnySubscriber<Element,Error> { [box] subscription in
-                    box.value = subscription
-                    sema.signal()
-                } receiveValue: { value in
-                    continuation.yield(value)
-                    return .none
-                } receiveCompletion: { completion in
-                    switch completion {
-                    case .finished:
-                        continuation.finish()
-                    case .failure(let error):
-                        continuation.finish(throwing: error)
-                    }
-                }
-                continuation.onTermination = { [box] event in
-                    if case .cancelled = event {
-                        box.value?.cancel()
-                    }
-                }
-
-                DispatchQueue.global().async { [source] in
-                    source.mapError{ $0 }.receive(subscriber: subscriber)
-                }
-            }.makeAsyncIterator()
-            sema.wait()
-            return iterator
-        }
-        
-    }
-    
-
- 
-}
-
-
-
 public struct CompatAsyncPublisher<P:Publisher>: AsyncSequence where P.Failure == Never {
 
     public typealias AsyncIterator = Iterator
@@ -170,59 +89,232 @@ public struct CompatAsyncPublisher<P:Publisher>: AsyncSequence where P.Failure =
     
     public struct Iterator: AsyncIteratorProtocol {
         
-        public typealias StoredType = Subscription & AnyObject
         public typealias Element = P.Output
         
         let source:P
-        private var base:AsyncStream<Element>.Iterator? = nil
-        private let box = RefBox<Subscription>()
+        private var subscriber:AsyncSubscriber?
         
         public mutating func next() async -> P.Output? {
-            if base == nil {
-                base = setup()
-            }
             if Task.isCancelled {
-                box.value?.cancel()
+                subscriber = nil
+                return nil
+            } else if let subscriber {
+                return await subscriber.awaitNext()
             } else {
-                box.value?.request(.max(1))
+                let a = AsyncSubscriber()
+                source.receive(subscriber: a)
+                subscriber = a
+                return await a.awaitNext()
             }
-            return await base?.next()
         }
         
         fileprivate init(source: P) {
             self.source = source
         }
         
-        private func setup() -> AsyncStream<Element>.Iterator {
-            let sema = DispatchSemaphore(value: 0)
+    }
+    
+    final class AsyncSubscriber: Subscriber {
+        
+        typealias Input = Element
+        
+        typealias Failure = Never
+        
+        private var token:AnyCancellable?
+        private var subscription:Subscription?
+        private let store = ContinuationStore()
+        func receive(_ input: Element) -> Subscribers.Demand {
+            store.mutate { list in
+                list.forEach { $0.resume(returning: input) }
+                list = []
+            }
+            return .none
+        }
+        
+        func receive(completion: Subscribers.Completion<Never>) {
+            store.mutate {
+                $0.forEach { continuation in continuation.resume(returning: nil) }
+                $0 = []
+                subscription = nil
+            }
+            token = nil
+        }
+        
 
-            let iterator = AsyncStream<Element>{ continuation in
-                let subscriber = AnySubscriber<Element,Never> { [box] subscription in
-                    box.value = subscription
-                    sema.signal()
-                } receiveValue: { value in
-                    continuation.yield(value)
-                    return .none
-                } receiveCompletion: { completion in
-                    switch completion {
-                    case .finished:
-                        continuation.finish()
-                    }
-                }
-                continuation.onTermination = { [box] event in
-                    if case .cancelled = event {
-                        box.value?.cancel()
-                    }
-                }
+        func awaitNext() async -> Element? {
+            return await withTaskCancellationHandler {
+                self.dispose()
+            } operation: {
+                let cancelled = Task.isCancelled
+               return await withUnsafeContinuation { continuation in
+                   if let subscription, !cancelled {
+                       store.mutate {
+                           $0.append(continuation)
+                       }
+                       subscription.request(.max(1))
 
-                DispatchQueue.global().async { [source] in
-                    source.receive(subscriber: subscriber)
+                   } else {
+                       continuation.resume(returning: nil)
+                   }
+
                 }
-            }.makeAsyncIterator()
-            sema.wait()
-            return iterator
+            }
+        }
+        
+        
+        func receive(subscription: Subscription) {
+            self.subscription = subscription
+
+            token = AnyCancellable(subscription)
+        }
+        
+        
+        private func dispose() {
+            token = nil
+            subscription = nil
+            store.mutate { list in
+                list.forEach{ $0.resume(returning: nil) }
+                list = []
+            }
+        }
+        
+        fileprivate init() {}
+    }
+
+    final class ContinuationStore {
+        private let lock = NSLock()
+        var list:[UnsafeContinuation<Element?,Never>] = []
+        func mutate(operation: (inout [UnsafeContinuation<Element?,Never>]) -> Void) {
+            lock.lock()
+            operation(&list)
+            lock.unlock()
+        }
+        fileprivate init() {}
+    }
+
+    
+}
+
+public struct CompatAsyncThrowingPublisher<P:Publisher>: AsyncSequence {
+
+    public typealias AsyncIterator = Iterator
+    public typealias Element = P.Output
+    
+    public var publisher:P
+    
+    public func makeAsyncIterator() -> AsyncIterator {
+        Iterator(source: publisher)
+    }
+    
+    public struct Iterator: AsyncIteratorProtocol {
+        
+        public typealias Element = P.Output
+        
+        let source:P
+        private var subscriber:AsyncThrowingSubscriber?
+        
+        public mutating func next() async throws -> P.Output? {
+            if Task.isCancelled {
+                subscriber = nil
+                return nil
+            } else if let subscriber {
+                return try await subscriber.awaitNext()
+            } else {
+                let a = AsyncThrowingSubscriber()
+                source.receive(subscriber: a)
+                subscriber = a
+                return try await a.awaitNext()
+            }
+        }
+        
+        fileprivate init(source: P) {
+            self.source = source
         }
         
     }
     
+    final class AsyncThrowingSubscriber: Subscriber{
+        
+        typealias Input = Element
+        
+        typealias Failure = P.Failure
+        
+        private var token:AnyCancellable?
+        private var subscription:Subscription?
+        private let store = ContinuationThrowingStore()
+        func receive(_ input: Element) -> Subscribers.Demand {
+            store.mutate { list in
+                list.forEach { $0.resume(returning: input) }
+                list = []
+            }
+            return .none
+        }
+        
+        func receive(completion: Subscribers.Completion<Failure>) {
+            store.mutate {
+                switch completion {
+                case .finished:
+                    $0.forEach { continuation in continuation.resume(returning: nil) }
+                case .failure(let failure):
+                    $0.forEach { continuation in continuation.resume(throwing: failure) }
+                }
+                $0 = []
+                subscription = nil
+            }
+        }
+        
+
+        func awaitNext() async throws -> Element? {
+            return try await withTaskCancellationHandler {
+                self.dispose()
+            } operation: {
+                let cancelled = Task.isCancelled
+               return try await withUnsafeThrowingContinuation { continuation in
+                   if let subscription, !cancelled {
+                       store.mutate {
+                           $0.append(continuation)
+                       }
+                       subscription.request(.max(1))
+
+                   } else {
+                       continuation.resume(returning: nil)
+                   }
+
+                }
+            }
+        }
+        
+        
+        func receive(subscription: Subscription) {
+            self.subscription = subscription
+            token = AnyCancellable(subscription)
+        }
+        
+        
+        private func dispose() {
+            token = nil
+            subscription = nil
+            store.mutate { list in
+                list.forEach{ $0.resume(returning: nil) }
+                list = []
+            }
+        }
+        
+        fileprivate init() {}
+    }
+
+    final class ContinuationThrowingStore {
+        private let lock = NSLock()
+        private var list:[UnsafeContinuation<Element?,Error>] = []
+        internal func mutate(operation: (inout [UnsafeContinuation<Element?,Error>]) -> Void) {
+            lock.lock()
+            operation(&list)
+            lock.unlock()
+        }
+        
+        fileprivate init() {}
+    }
+
+    
 }
+
